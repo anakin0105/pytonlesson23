55baf82e0192d6d45aefab726014fea5afe8de87
Анна Акиньшина <anakin0105@gmail.com>
2026-03-26 08:50:01 +0300

    Сделана задача урока 23

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
-
 
 app_name = CatalogConfig.name
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+    path("", home, name="home"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
+
+# ====================== MEDIA FILES (чтобы фото показывались) ======================
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
